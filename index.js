2e2447f2c06637f7258c8fd13a43dd859cee90ed
ch Nitheesh <chalumuri.nitheesh@gmail.com>
2024-12-03 19:42:40 +0530

    sign in done succesful

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const { Server } = require("socket.io");
 
-app.set("view engine","ejs")
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-main().then(()=>{console.log("connection successful");}).catch((err)=>{console.log(err)})
-async function main() {
-await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp_users");}
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.render("home.ejs");})
-app.listen("3000",()=>{
-    console.log("app is listening");})
-const chatschema = new mongoose.Schema({
-    from:String,
-    to:String,
-})
-const chat = mongoose.model("chat",chatschema);
-let chat1 = new chat({from:"nitheesh",to:"pranathi"});
-chat1.save();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/whatsapp_users")
+    .then(() => console.log("Connection successful"))
+    .catch((err) => console.error("Database connection error:", err));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+const server = app.listen(3000, () => {
+    console.log("App is listening on port 3000");
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("message", (data) => {
+        console.log("Message received:", data);
+        socket.emit("reply", "Hello from the server!");
+    });
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+const userschema = new mongoose.Schema({
+    name: String,
+    password: String,
+});
+
+const user = mongoose.model("user", userschema);
+
+app.post("/signup", async (req, res) => {
+    const { name, password } = req.body;
+    try {
+        const existingUser = await user.findOne({ name: name });
+        if (existingUser) {
+            console.log(`User with name ${name} already exists`);
+            return res.status(400).send("User already exists");
+        }
+        const newUser = new user({ name: name, password: password });
+        await newUser.save();
+
+        console.log(`User ${name} signed up successfully`);
+        res.render("view", { name });
+    } catch (err) {
+        console.error("Error during signup:", err);
+        res.status(500).send("Error during signup");
+    }
+});
---
+++ public/script.js
@@ -0,0 +1,9 @@
+// // import {io} from 'socket.io-client';
+// const { io } = require("socket.io-client");
+
+// console.log("hello");
+// const socket = io("http://localhost:3000");
+
+// socket.on("connection", () => {
+//     console.log("connected very good");
+// });
